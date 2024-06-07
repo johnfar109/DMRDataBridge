61dc59be83a1e0b4dabba1e032ea9b4f308b64f2
John Farnach <Johnfar109@gmail.com>
2024-06-07 00:00:37 -0400

    Cleanup of Login Logic Now preforming checks on Response Msgs

--- DMRDataBridge/MainDisplay.Designer.cs
+++ DMRDataBridge/MainDisplay.Designer.cs
@@ -31,6 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.btnConnect = new System.Windows.Forms.Button();
             this.tmrPing = new System.Windows.Forms.Timer(this.components);
+            this.toolStripMain = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabelConStatus = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabelPing = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -48,16 +53,50 @@
             this.tmrPing.Interval = 5000;
             this.tmrPing.Tick += new System.EventHandler(this.tmrPing_Tick);
             // 
+            // toolStripMain
+            // 
+            this.toolStripMain.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabelConStatus,
+            this.toolStripSeparator1,
+            this.toolStripLabelPing});
+            this.toolStripMain.Location = new System.Drawing.Point(0, 311);
+            this.toolStripMain.Name = "toolStripMain";
+            this.toolStripMain.Size = new System.Drawing.Size(293, 25);
+            this.toolStripMain.TabIndex = 2;
+            this.toolStripMain.Text = "toolStrip1";
+            // 
+            // toolStripLabelConStatus
+            // 
+            this.toolStripLabelConStatus.Name = "toolStripLabelConStatus";
+            this.toolStripLabelConStatus.Size = new System.Drawing.Size(79, 22);
+            this.toolStripLabelConStatus.Text = "Disconnected";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabelPing
+            // 
+            this.toolStripLabelPing.Name = "toolStripLabelPing";
+            this.toolStripLabelPing.Size = new System.Drawing.Size(57, 22);
+            this.toolStripLabelPing.Text = "Waiting...";
+            // 
             // MainDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(293, 125);
+            this.ClientSize = new System.Drawing.Size(293, 336);
+            this.Controls.Add(this.toolStripMain);
             this.Controls.Add(this.btnConnect);
             this.Name = "MainDisplay";
             this.Text = "DMR Data Bridge";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainDisplay_FormClosed);
+            this.toolStripMain.ResumeLayout(false);
+            this.toolStripMain.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -65,6 +104,10 @@
 
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Timer tmrPing;
+        private System.Windows.Forms.ToolStrip toolStripMain;
+        private System.Windows.Forms.ToolStripLabel toolStripLabelConStatus;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabelPing;
     }
 }
 
--- DMRDataBridge/MainDisplay.cs
+++ DMRDataBridge/MainDisplay.cs
@@ -22,6 +22,8 @@ namespace DMRDataBridge
 
         private bool _connected = false;
 
+        private bool _pingState = false;
+
         public MainDisplay()
         {
             InitializeComponent();
@@ -32,10 +34,23 @@ namespace DMRDataBridge
             //setup UDP Socket
             _udpClient = new UdpClient();
             IPEndPoint _ep = new IPEndPoint(IPAddress.Parse(Properties.Settings.Default.Host), Properties.Settings.Default.HostPort); // endpoint where server is listening
+
+
+            _ep = Connect(_ep);
+
+        }
+
+        private IPEndPoint Connect(IPEndPoint _ep)
+        {
             // open the port 
             _udpClient.Connect(_ep);
 
+            bool doPhase2 = false;
+            bool doPhase3 = false;
+            bool loginGood = false;
+
             // ## Phase 1 ##
+            toolStripLabelConStatus.Text = "Login Phase 1";
             // Form and send Login Phase 1 Msg
             byte[] phase1Cmd = Encoding.ASCII.GetBytes("RPTL").Concat(BitConverter.GetBytes(Properties.Settings.Default.StationID).Reverse().ToArray()).ToArray();
 
@@ -45,56 +60,142 @@ namespace DMRDataBridge
             // Read Response Body Login Phase 1
             var phase1RespBody = _udpClient.Receive(ref _ep);
 
-            // Parse Response Body - Login Phase 1
-            var phase1RespHead = Encoding.ASCII.GetString(phase1RespBody, 0, 6);
-            _phase1RespSalt = BitConverter.ToUInt32(phase1RespBody.Skip(6).Reverse().ToArray(), 0);
+            if (phase1RespBody.Length > 0)
+            {
+                // Parse Response Body - Login Phase 1
+                var phase1RespHead = Encoding.ASCII.GetString(phase1RespBody, 0, 6);
+                if ("RPTACK" == phase1RespHead)
+                {
+                    _phase1RespSalt = BitConverter.ToUInt32(phase1RespBody.Skip(6).Reverse().ToArray(), 0);
+                    doPhase2 = true;
+                }
+                else if ("RPTNAK" == phase1RespHead)
+                {
+                    toolStripLabelConStatus.Text = "Login Phase 1: Rejected";
+                }
+                else
+                {
+                    toolStripLabelConStatus.Text = "Login Phase 1: Unknown Response";
+                }
+            }
+            else
+            {
+                toolStripLabelConStatus.Text = "Login Phase 1: No Response";
+            }
 
-            // ## Phase 2 ##
-            // Form and send Login Phase 2 Msg
-            byte[] phase2Secret = BitConverter.GetBytes(_phase1RespSalt).Reverse().ToArray().Concat(Encoding.ASCII.GetBytes(Properties.Settings.Default.Password)).ToArray();
-            SHA256Managed hashstring = new SHA256Managed();
-            byte[] secretHash = hashstring.ComputeHash(phase2Secret);
+            if (doPhase2)
+            {
+                // ## Phase 2 ##
+                toolStripLabelConStatus.Text = "Login Phase 2";
+                // Form and send Login Phase 2 Msg
+                byte[] phase2Secret = BitConverter.GetBytes(_phase1RespSalt).Reverse().ToArray().Concat(Encoding.ASCII.GetBytes(Properties.Settings.Default.Password)).ToArray();
+                SHA256Managed hashstring = new SHA256Managed();
+                byte[] secretHash = hashstring.ComputeHash(phase2Secret);
 
-            byte[] phase2Cmd = Encoding.ASCII.GetBytes("RPTK").Concat(BitConverter.GetBytes(Properties.Settings.Default.StationID).Reverse().ToArray()).Concat(secretHash).ToArray();
+                byte[] phase2Cmd = Encoding.ASCII.GetBytes("RPTK").Concat(BitConverter.GetBytes(Properties.Settings.Default.StationID).Reverse().ToArray()).Concat(secretHash).ToArray();
 
-            // Send Phase 2 Msg
-            _udpClient.Send(phase2Cmd, phase2Cmd.Length);
+                // Send Phase 2 Msg
+                _udpClient.Send(phase2Cmd, phase2Cmd.Length);
 
-            // Read Response Body Login Phase 2
-            var phase2RespBody = _udpClient.Receive(ref _ep);
+                // Read Response Body Login Phase 2
+                var phase2RespBody = _udpClient.Receive(ref _ep);
 
-            // Parse Response Body - Login Phase 2
-            var phase2RespHead = Encoding.ASCII.GetString(phase2RespBody, 0, 6);
-            var phase2RespStationId = BitConverter.ToUInt32(phase2RespBody.Skip(6).Reverse().ToArray(), 0);
+                if (phase2RespBody.Length > 0)
+                {
+                    // Parse Response Body - Login Phase 2
+                    var phase2RespHead = Encoding.ASCII.GetString(phase2RespBody, 0, 6);
+                    if ("RPTACK" == phase2RespHead)
+                    {
+                        var phase2RespStationId = BitConverter.ToUInt32(phase2RespBody.Skip(6).Reverse().ToArray(), 0);
+                        if (Properties.Settings.Default.StationID == phase2RespStationId)
+                        {
+                            doPhase3 = true;
+                        }
+                        else
+                        {
+                            toolStripLabelConStatus.Text = "Login Phase 2: Station Id Missmatch";
+                        }
+                    }
+                    else if ("RPTNAK" == phase2RespHead)
+                    {
+                        toolStripLabelConStatus.Text = "Login Phase 2: Rejected";
+                    }
+                    else
+                    {
+                        toolStripLabelConStatus.Text = "Login Phase 2: Unknown Response";
+                    }
+                }
+                else
+                {
+                    toolStripLabelConStatus.Text = "Login Phase 2: No Response";
+                }
+            }
 
-            // ## Phase 3 ##
-            // send Login phase 3 (Conig Data)
-            byte[] phase3Cmd = Encoding.ASCII.GetBytes("RPTC").Concat(BitConverter.GetBytes(Properties.Settings.Default.StationID).Reverse().ToArray()).Concat(Encoding.ASCII.GetBytes(Properties.Settings.Default.Callsign)).ToArray();
-            // TODO Pack the rest of the station Config Data
+            if (doPhase3)
+            {
+                // ## Phase 3 ##
+                toolStripLabelConStatus.Text = "Login Phase 3";
+                // send Login phase 3 (Conig Data)
+                byte[] phase3Cmd = Encoding.ASCII.GetBytes("RPTC").Concat(BitConverter.GetBytes(Properties.Settings.Default.StationID).Reverse().ToArray()).Concat(Encoding.ASCII.GetBytes(Properties.Settings.Default.Callsign)).ToArray();
+                // TODO Pack the rest of the station Config Data
 
-            // Send Phase 3 Msg
-            _udpClient.Send(phase3Cmd, phase3Cmd.Length);
+                // Send Phase 3 Msg
+                _udpClient.Send(phase3Cmd, phase3Cmd.Length);
 
-            // Read Response Body Login Phase 3
-            var phase3RespBody = _udpClient.Receive(ref _ep);
+                // Read Response Body Login Phase 3
+                var phase3RespBody = _udpClient.Receive(ref _ep);
 
-            // Parse Response Body - Login Phase 3
-            var phase3RespHead = Encoding.ASCII.GetString(phase3RespBody, 0, 6);
-            var phase3RespStationId = BitConverter.ToUInt32(phase3RespBody.Skip(6).Reverse().ToArray(), 0);
+                if (phase3RespBody.Length > 0)
+                {
+                    // Parse Response Body - Login Phase 3
+                    var phase3RespHead = Encoding.ASCII.GetString(phase3RespBody, 0, 6);
+                    if ("RPTACK" == phase3RespHead)
+                    {
+                        var phase3RespStationId = BitConverter.ToUInt32(phase3RespBody.Skip(6).Reverse().ToArray(), 0);
+                        if (Properties.Settings.Default.StationID == phase3RespStationId)
+                        {
+                            loginGood = true;
+                        }
+                        else
+                        {
+                            toolStripLabelConStatus.Text = "Login Phase 3: Station Id Missmatch";
+                        }
+                    }
+                    else if ("RPTNAK" == phase3RespHead)
+                    {
+                        toolStripLabelConStatus.Text = "Login Phase 3: Rejected";
+                    }
+                    else
+                    {
+                        toolStripLabelConStatus.Text = "Login Phase 3: Unknown Response";
+                    }
 
+                }
+                else
+                {
+                    toolStripLabelConStatus.Text = "Login Phase 3: No Response";
+                }
+            }
 
-            _connected = true;
-            tmrPing.Interval = Properties.Settings.Default.KeepAliveInterval;
-            tmrPing.Enabled = true;
+            if (loginGood)
+            {
+                _connected = true;
+                toolStripLabelConStatus.Text = "Connected";
+                toolStripLabelPing.Text = "Ping";
+                btnConnect.Enabled = false;
+                tmrPing.Interval = Properties.Settings.Default.KeepAliveInterval;
+                tmrPing.Enabled = true;
+            }
 
-            var i = 0;
-
+            return _ep;
         }
 
         private void tmrPing_Tick(object sender, EventArgs e)
         {
             if (_connected)
             {
+                bool pingGood = false;
+                
                 // ## Ping ##
                 // send Ping
                 byte[] pingCmd = Encoding.ASCII.GetBytes("RPTPING").Concat(BitConverter.GetBytes(Properties.Settings.Default.StationID).Reverse().ToArray()).ToArray();
@@ -105,24 +206,84 @@ namespace DMRDataBridge
                 // Read Response Body Ping
                 var pingRespBody = _udpClient.Receive(ref _ep);
 
-                // Parse Response Body - Ping
-                var pingRespHead = Encoding.ASCII.GetString(pingRespBody, 0, 7);
-                var pingRespStationId = BitConverter.ToUInt32(pingRespBody.Skip(7).Reverse().ToArray(), 0);
+                if (pingRespBody.Length > 0)
+                {
+                    // Parse Response Body - Ping
 
-                var pingRespHeadNak = Encoding.ASCII.GetString(pingRespBody, 0, 6);
-                var pingRespStationIdNak = BitConverter.ToUInt32(pingRespBody.Skip(6).Reverse().ToArray(), 0);
+                    var pingRespHeadNak = Encoding.ASCII.GetString(pingRespBody, 0, 6);
+                    if ("MSTNAK" == pingRespHeadNak)
+                    {
+                        var pingRespStationIdNak = BitConverter.ToUInt32(pingRespBody.Skip(6).Reverse().ToArray(), 0);
+                        if (Properties.Settings.Default.StationID == pingRespStationIdNak)
+                        {
+                            toolStripLabelPing.Text = "Ping: NAK";
+                        }
+                        else
+                        {
+                            toolStripLabelPing.Text = "Ping: NAK - Station Id Missmatch";
+                        }
+                    }
+                    else
+                    {
+                        var pingRespHead = Encoding.ASCII.GetString(pingRespBody, 0, 7);
+                        if ("MSTPONG" == pingRespHead)
+                        {
+                            var pingRespStationId = BitConverter.ToUInt32(pingRespBody.Skip(7).Reverse().ToArray(), 0);
+                            if (Properties.Settings.Default.StationID == pingRespStationId)
+                            {
+                                pingGood = true;
+                            }
+                            else
+                            {
+                                toolStripLabelPing.Text = "Ping: Station Id Missmatch";
+                            }
+                        }
+                        else
+                        {
+                            toolStripLabelPing.Text = "Ping: Unknown Response";
+                        }
+                    }
 
+                }
+                else
+                {
+                    toolStripLabelPing.Text = "Ping: No Response";
+                }
+
+                if (pingGood)
+                {
+                    if (_pingState)
+                    {
+                        toolStripLabelPing.Text = "Ping";
+                    }
+                    else
+                    {
+                        toolStripLabelPing.Text = "Pong";
+                    }
+                    _pingState = !_pingState;
+                }
+                else
+                {
+                    Disconnect();
+                }
             }
         }
 
         private void MainDisplay_FormClosed(object sender, FormClosedEventArgs e)
         {
-            tmrPing.Enabled = false;
-
             if (_connected)
             {
-                _udpClient.Close();
+                Disconnect();
             }
+        }
+
+        private void Disconnect()
+        {
+            _connected = false;
+            tmrPing.Enabled = false;
+            _udpClient.Close();
+            toolStripLabelConStatus.Text = "Disconnected";
+            btnConnect.Enabled = true;
         }
 
     }
