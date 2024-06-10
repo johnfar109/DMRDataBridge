97a826ee4f36565939af373215082a686fb6b2f4
John Farnach <Johnfar109@gmail.com>
2024-06-09 22:39:28 -0400

    Added Caly Cycle Data Struct.

---
+++ DMRDataBridge/CallCycleData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMRDataBridge
+{
+
+    public enum CallState : byte
+    {
+        Waiting,
+        Active,
+        Timeout
+    }
+
+    public struct CallCycleData
+    {
+        public CallState CurrentCall { get; set; }
+        public uint StreamId { get; set; }
+        public uint SrcId { get; set; }
+        public uint DstId { get; set; }
+        public int SlotNo { get; set; }
+        public PacketType LastType { get; set; }
+        public int LastSeq { get; set; }
+        public int PacketCount { get; set; }
+        public List<int> BER { get; set; }
+        public List<int> RSSI { get; set; }
+        public int TimeoutCount { get; set; }
+
+    }
+}
