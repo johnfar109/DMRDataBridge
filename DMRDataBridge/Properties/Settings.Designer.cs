e4c0eb2c047e0639e6222e805d52b8320d81ad75
John Farnach <johnfar109@gmail.com>
2026-05-16 00:55:58 -0400

    Pull forward to .NET 4.8, VS2026 Updated default settings to match local dev

--- DMRDataBridge/Properties/Settings.Designer.cs
+++ DMRDataBridge/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DMRDataBridge.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace DMRDataBridge.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("10.10.1.106")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10.10.1.9")]
         public string Host {
             get {
                 return ((string)(this["Host"]));
@@ -61,7 +61,7 @@ namespace DMRDataBridge.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("313541202")]
+        [global::System.Configuration.DefaultSettingValueAttribute("313541204")]
         public uint StationID {
             get {
                 return ((uint)(this["StationID"]));
